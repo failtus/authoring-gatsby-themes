b9bf896c2cedcf3e8f1e5deafbc5cee7dd62e883
Failtus <iamfailtus@gmail.com>
2019-07-10 02:09:16 +0600

    feat: Style and Format Dates in React

--- gatsby-theme-events/src/components/event.js
+++ gatsby-theme-events/src/components/event.js
@@ -1,10 +1,41 @@
 import React from 'react';
 
+const getDate = (date, { day = true, month = true, year = true } = {}) => {
+  return date.toLocaleDateString('en-BD', {
+    day: day ? 'numeric' : undefined,
+    month: month ? 'long' : undefined,
+    year: year ? 'numeric' : undefined
+  });
+}
+
+
+const EventDate = ({ startDate, endDate }) => {
+  const start = new Date(startDate);
+  const end = new Date(endDate);
+
+  const isOneDay = start.toDateString() === end.toDateString();
+
+  return (
+    <>
+      <time datetime={start.toISOString()}>{getDate(start, { year: isOneDay })}</time>
+      {!isOneDay && (
+        <>
+          -
+          <time datetime={end.toISOString()}>
+            {getDate(end, { month: start.getMonth() !== end.getMonth() })}
+          </time>
+        </>
+      )}
+    </>
+
+  )
+}
+
 const Event = ({ name, location, url, startDate, endDate }) => (
   <div>
     <h1>{name} ({location})</h1>
     <p>
-      {startDate} - {endDate}
+      <EventDate startDate={startDate} endDate={endDate} />
     </p>
     <p>
       Website: <a href={url}>{url}</a>
